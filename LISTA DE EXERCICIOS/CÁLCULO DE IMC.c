299f96e3959364ce36050b40c5d919faf7bbf1c0
Ep1l3f <santos.luizfeliperh@gmail.com>
2026-03-14 10:55:17 -0300

    Add files via upload

---
+++ LISTA DE EXERCICIOS/CÁLCULO DE IMC.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+	
+	float altura;
+ 	float peso;
+ 	float resultado_imc;
+ 	
+ 	
+ 	printf("Digite a sua altura em metros (Utilize o ponto inves da virgula ex: 1.75):\n");
+ 	scanf("%f", &altura);
+ 	
+ 	printf("Digite o seu peso em kg (Utilize o ponto inves da virgula ex: 75.5):\n");
+ 	scanf("%f", &peso);
+ 	
+		
+	resultado_imc = peso / (altura * altura);
+	
+	printf("O resultado do seu IMC sera de: %.2f\n", resultado_imc);
+		
+	
+		
+		return 0;
+		
+		
+}
